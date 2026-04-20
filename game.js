cb5463491f370fa05d48ff96317a7b646dc1d712
Matthew Xia <matthewxia@puppygraph.com>
2026-04-20 01:57:16 -0700

    update arm/sword to match

--- game.js
+++ game.js
@@ -381,7 +381,7 @@ function buildPlayerContainer(c, gameClass) {
   // to make it perpendicular. Blade along Y axis, handle at top.
   const sword = new PIXI.Sprite(texCache.sword);
   sword.anchor.set(0.5, 0.5);
-  sword.x = 30; sword.y = -45;   // sits in front of player
+  sword.x = 30; sword.y = -30;   // sits in front of player
   sword.scale.set(0.09, 0.11);
   sword.rotation = - Math.PI / 2; // rotate 90deg so blade is perpendicular to facing
   //sword.blendMode = PIXI.BLEND_MODES.ADD;
@@ -392,14 +392,14 @@ function buildPlayerContainer(c, gameClass) {
   const arm1 = new PIXI.Graphics();
   arm1.lineStyle(2, 0x000000, 0.5);
   arm1.beginFill(st.arm, 1);
-  arm1.drawCircle(18, -15, 7);  // forward, slightly up (one hand on handle)
+  arm1.drawCircle(18, -12, 7);  // forward, slightly up (one hand on handle)
   arm1.endFill();
   arm1.name = 'arm1'; c.addChild(arm1);
 
   const arm2 = new PIXI.Graphics();
   arm2.lineStyle(2, 0x000000, 0.5);
   arm2.beginFill(st.arm, 1);
-  arm2.drawCircle(18, 15, 7);   // forward, slightly down (other hand on handle)
+  arm2.drawCircle(18, 12, 7);   // forward, slightly down (other hand on handle)
   arm2.endFill();
   arm2.name = 'arm2'; c.addChild(arm2);
 
