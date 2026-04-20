52f767289db29a951300a69fd202c52f42e2d676
d1 <159611786+d1skidder@users.noreply.github.com>
2026-04-19 21:34:10 -0700

    Update game.js

--- game.js
+++ game.js
@@ -406,7 +406,7 @@ function buildPlayerContainer(c, gameClass) {
   const sword = new PIXI.Sprite(texCache.sword);
   sword.anchor.set(0.5, 0.5);
   sword.x = 18; sword.y = 0;   // sits in front of player
-  sword.width = 0.1; sword.height = 100;
+  sword.width = 0.01; sword.height = 50;
   sword.rotation = Math.PI / 2; // rotate 90deg so blade is perpendicular to facing
   //sword.blendMode = PIXI.BLEND_MODES.ADD;
   sword.name = 'sword'; c.addChild(sword);
